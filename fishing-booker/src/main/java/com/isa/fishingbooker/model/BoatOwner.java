b4b0dc9a1585b2ff9bdfee15a4c339e9a83b7bfb
Mlaki1999 <mladenpavlovic1999@gmail.com>
2021-11-22 21:48:15 +0100

    proba grane

--- fishing-booker/src/main/java/com/isa/fishingbooker/model/BoatOwner.java
+++ fishing-booker/src/main/java/com/isa/fishingbooker/model/BoatOwner.java
@@ -63,8 +63,7 @@ public class BoatOwner {
 	@Column(name = "explanation")
 	private String explanation;
 	
-	@Column(name = "activated")
-	private String activated;
+	
 	
 	@JsonIgnore
     @OneToMany(mappedBy="boatOwner",fetch=FetchType.LAZY)
