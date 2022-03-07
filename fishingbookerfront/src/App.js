57b7df373562fb44cddbd982d3ab316141a06d0f
Savkelaa <savic.in35.2018@uns.ac.rs>
2022-03-07 21:37:03 +0100

    added login page

--- fishingbookerfront/src/App.js
+++ fishingbookerfront/src/App.js
@@ -10,6 +10,7 @@ import FishingClassesContainer from "./Containers/FishingClassesContainer";
 import FishingClassContainer from "./Containers/FishingClassContainer";
 import CreateFishingClassReservationContainer from "./Containers/CreateFishingClassReservationContainer";
 import InstructorHomePageContainer from "./Containers/InstructorHomePageContainer";
+import LoginContainer from "./Containers/LoginContainer";
 
 function App() {
   return (
@@ -21,6 +22,9 @@ function App() {
           </Route>
           <Route path="/fishingClasses">
             <FishingClassesContainer></FishingClassesContainer>
+          </Route>
+          <Route path="/login">
+            <LoginContainer></LoginContainer>
           </Route>
           <Route path="/createQuickReservation">
             <CreateFishingClassReservationContainer></CreateFishingClassReservationContainer>
