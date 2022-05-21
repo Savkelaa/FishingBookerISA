de9a26e49b7d79f00c6bb9cc7caf5231aeb9da35
Savkelaa <savic.in35.2018@uns.ac.rs>
2022-05-21 16:46:39 +0200

    dodato na backu

--- fishing-booker/src/main/java/com/isa/fishingbooker/controller/DateSpanController.java
+++ fishing-booker/src/main/java/com/isa/fishingbooker/controller/DateSpanController.java
@@ -1,15 +1,11 @@
 package com.isa.fishingbooker.controller;
 
-import com.isa.fishingbooker.model.BoatRate;
-import com.isa.fishingbooker.model.DateSpan;
-import com.isa.fishingbooker.model.FishingClass;
-import com.isa.fishingbooker.service.BoatRateService;
+import com.isa.fishingbooker.model.DateSpanInstructor;
 import com.isa.fishingbooker.service.DateSpanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
-import java.util.Date;
 import java.util.List;
 @CrossOrigin
 @RestController
@@ -20,18 +16,18 @@ public class DateSpanController {
     private DateSpanService dateSpanService;
 
     @GetMapping("/dateSpan")
-    public List<DateSpan> getAllDateSpans() {
+    public List<DateSpanInstructor> getAllDateSpans() {
         return this.dateSpanService.getAllDateSpans();
     }
 
     @GetMapping("/dateSpanInstructor")
-    public List<DateSpan> getDateSpansByInstructor(@RequestParam (value="instructorId")  Integer instructorId){
+    public List<DateSpanInstructor> getDateSpansByInstructor(@RequestParam (value="instructorId")  Integer instructorId){
         return dateSpanService.getDateSpansByInstructor(instructorId);
     }
 
     @PostMapping("/dateSpan")
-    public DateSpan createDateSpan(@RequestBody DateSpan dateSpan) {
-        return dateSpanService.createDateSpan(dateSpan);
+    public DateSpanInstructor createDateSpan(@RequestBody DateSpanInstructor dateSpanInstructor) {
+        return dateSpanService.createDateSpan(dateSpanInstructor);
     }
 
 
