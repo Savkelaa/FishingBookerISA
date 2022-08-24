bfa2ac161dd9dc360ccada99207074297fae859f
mlaki99 <mlakidjomla99@gmail.com>
2022-08-24 22:13:44 +0200

    Rate and complaint for ordinary reservations

---
+++ fishingbookerfront/src/Containers/CottageOrdinaryReservationHistoryContainer.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import Footerr from "../Components/Common/Footerr";
+import Navbarr from "../Components/Common/Navbarr";
+import Request from "../Components/Common/Request";
+import cottageQuickReservationServices from "../Services/CottageQuickReservationServices/CottageQuickReservationServices";
+import userServices from "../Services/UserServices/UserServices";
+import CottageReservationHistory from "../Components/Common/CottageReservationHistory";
+import CottageReservationHistoryClientSide from "../Components/Common/CottageOrdinaryReservationHistory"
+
+export default function CottageReservationHistoryContainer() {
+  const [finishedReservations, setFinishedReservations] = useState([]);
+
+  var logedClient = JSON.parse(localStorage.getItem("Client"));
+
+  useEffect(() => {
+    cottageQuickReservationServices
+    .getAllCottageReservationsByClient(logedClient.id)
+      .then((data) => {
+        setFinishedReservations(data.data);
+      })
+      .catch((error) => console.log(`error`, error));
+  }, []);
+
+  return (
+    <div>
+      <Navbarr></Navbarr>
+      <CottageReservationHistoryClientSide
+        finishedReservations={finishedReservations}
+      ></CottageReservationHistoryClientSide>
+      <Footerr></Footerr>
+    </div>
+  );
+}
